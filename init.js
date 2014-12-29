b257113a11112a594d454c88ee89850dcff5c577
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2014-12-29 16:29:01 -0500

    first stable base

---
+++ init.js
@@ -0,0 +1,19 @@
+var init = {
+	title: 'Blist: A blog powered by gists',
+	navbar: [ 
+		{ 
+			text: 'Fork me',
+			link: 'https://github.com/Gioyik/Blist' 
+		},
+		{ 
+			text: 'Docs',
+			link: 'https://github.com/sankha93/grite/wiki/Documentation'
+		} 
+	],
+	posts: [ 
+		'0b30129228ca1f271c3e',
+		'84cdcb2284205d7c14eb'
+	],
+	atOnce: 5,
+	footerText: "Blist is a minimalistic blog maintained with gists. Made by <a href=\"https://github.com/Gioyik\">@Gioyik</a>."
+};
---
+++ js/blist.js
@@ -0,0 +1,75 @@
+var queue = 0;
+var toBeLoaded = 0;
+var loaded = 0;
+
+function setTitle() {
+  $('#title').html(init.title);
+  document.title = init.title;
+}
+
+function setNavbar() {
+  for (var i = 0; i < init.navbar.length; i++) {
+    var dom = '<a href="' + init.navbar[i].link + '">' + init.navbar[i].text + '</a>';
+    if (i != 0)
+      $('<span> • </span>').appendTo('#navbar');
+    $(dom).appendTo('#navbar');
+  }
+}
+
+function createPlaceholders() {
+  for (var i = 0; i < init.atOnce; i++) {
+    if (toBeLoaded != init.posts.length) {
+      var dom = '<div id="post' + init.posts[toBeLoaded] + '"></div>';
+      $(dom).appendTo('#posts');
+      toBeLoaded++;
+    } else
+      break;
+  }
+}
+
+function loadPosts() {
+  $('#loader').show();
+  for (;loaded < toBeLoaded; loaded++) {
+    queue ++;
+    $.get('https://api.github.com/gists/' + init.posts[loaded], function (data) {
+      var gist = '';
+      for (var j in data.files)
+        gist = gist + marked.parse(data.files[j].content);
+      var dom = '<div class="post">' +
+                '<div class="post-text">' + gist + '</div>' +
+                '<span class="date"><a href="' + data.html_url + '">' + new Date(data.updated_at) + '</a></span>' +
+                ' • <span class="author">Posted by <a href="https://github.com/' + data.owner.login + '">' + data.owner.login + '</a></span>' +
+                '</div><hr>';
+      $(dom).appendTo('#post' + data.id);
+    }, 'json').done(function() {
+      queue --;
+      if (queue == 0)
+        $('#loader').hide();
+    });
+  }
+}
+
+function loadFooter() {
+  $('#footer-text').html(init.footerText);
+}
+
+function test() {
+  alert("nicolas es un tonto");
+}
+
+function loadOlder() {
+  createPlaceholders();
+  loadPosts();
+  if (toBeLoaded == init.posts.length)
+    $('#old-posts').hide();
+}
+
+$('#loader').hide();
+setTitle();
+setNavbar();
+loadFooter();
+$('#old-posts').bind('click', loadOlder);
+createPlaceholders();
+loadPosts();
+if (toBeLoaded == init.posts.length)
+  $('#old-posts').hide();
